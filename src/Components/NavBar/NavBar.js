d81781124e50e07ca764fc85cee60a7e61733aa9
pcinfotech <66895937+aswinpc143@users.noreply.github.com>
2025-01-17 09:31:18 +0530

    Update NavBar.js
    logo folder change

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react'
 function NavBar() {
     return (
         <div className="navbar">
-        <img className="logo" src={require('./logo/cooltext388381261994218.png')} alt='netflix-logo'/>
+        <img className="logo" src={require('./cooltext388381261994218.png')} alt='netflix-logo'/>
         <img className="avatar" src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" alt="Avatar" />
         </div>
     )
